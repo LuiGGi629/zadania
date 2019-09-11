4b20174012dc6783c5edc0d93027af960f33ae9d
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-09-11 09:46:54 +0200

    nested list comprehension

--- add.py
+++ add.py
@@ -1,8 +1,6 @@
 def add(matrix1, matrix2):
     """Add corresponding numbers in given 2-D matrices."""
-    combined = []
-    for row1, row2 in zip(matrix1, matrix2):
-        combined.append([
-            n + m
-            for n, m in zip(row1, row2)])
-    return combined
+    return [
+        [n + m for n, m in zip(row1, row2)]
+        for row1, row2 in zip(matrix1, matrix2)
+    ]
