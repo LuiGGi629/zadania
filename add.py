e836bcaa70553cf7658ccfa1fdcfec7389494b33
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-09-11 09:16:35 +0200

    multiple assignment instead of hard-coding indexes for readability

--- add.py
+++ add.py
@@ -1,9 +1,9 @@
 def add(matrix1, matrix2):
     """Add corresponding numbers in given 2-D matrices."""
     combined = []
-    for rows in zip(matrix1, matrix2):
+    for row1, row2 in zip(matrix1, matrix2):
         row = []
-        for items in zip(rows[0], rows[1]):
-            row.append(items[0] + items[1])
+        for n, m in zip(row1, row2):
+            row.append(n + m)
         combined.append(row)
     return combined
