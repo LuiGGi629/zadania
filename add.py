7cf2c55a3c8fcee00e54a4082a1278a5f1e7ab21
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-09-11 11:08:32 +0200

    catching a TypeError to handle the case where matrices aren't same size

--- add.py
+++ add.py
@@ -1,12 +1,12 @@
+from itertools import zip_longest
+
+
 def add(*matrices):
     """Add corresponding numbers in given 2-D matrices."""
-    matrix_shape = {
-        tuple(len(r) for r in matrix)
-        for matrix in matrices
-    }
-    if len(matrix_shape) > 1:
+    try:
+        return [
+            [sum(values) for values in zip_longest(*rows)]
+            for rows in zip_longest(*matrices)
+        ]
+    except TypeError as e:
         raise ValueError("Given matrices are not the same size.")
-    return [
-        [sum(values) for values in zip(*rows)]
-        for rows in zip(*matrices)
-    ]
