a3a9acc2e5e509860c8718930e0bc273590f7d31
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-09-11 10:04:13 +0200

    raise ValueError when lists-of-lists are of different shapes

--- add.py
+++ add.py
@@ -1,6 +1,8 @@
-def add(*matrices):
+def add(matrix1, matrix2):
     """Add corresponding numbers in given 2-D matrices."""
+    if [len(r) for r in matrix1] != [len(r) for r in matrix2]:
+        raise ValueError("Given matrices are not the same size.")
     return [
-        [sum(values) for values in zip(*rows)]
-        for rows in zip(*matrices)
+        [n + m for n, m in zip(row1, row2)]
+        for row1, row2 in zip(matrix1, matrix2)
     ]
