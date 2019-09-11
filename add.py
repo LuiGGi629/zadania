1ddc8c4ce02e2c1c180067be8585b4a1ea142869
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-09-11 09:12:22 +0200

    using zip func to loop over two lists at the same time

--- add.py
+++ add.py
@@ -1,9 +1,9 @@
 def add(matrix1, matrix2):
     """Add corresponding numbers in given 2-D matrices."""
     combined = []
-    for i in range(len(matrix1)):
+    for rows in zip(matrix1, matrix2):
         row = []
-        for j in range(len(matrix1[i])):
-            row.append(matrix1[i][j] + matrix2[i][j])
+        for items in zip(rows[0], rows[1]):
+            row.append(items[0] + items[1])
         combined.append(row)
     return combined
