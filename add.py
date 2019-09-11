cbd96d9c725240daded1cdc7e559334eb4524247
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-09-11 09:57:50 +0200

    into nested comprehension thanks to sum and zip functions

--- add.py
+++ add.py
@@ -1,9 +1,6 @@
 def add(*matrices):
     """Add corresponding numbers in given 2-D matrices."""
-    combined = []
-    for rows in zip(*matrices):
-        row = []
-        for values in zip(*rows):
-            row.append(sum(values))
-        combined.append(row)
-    return combined
+    return [
+        [sum(values) for values in zip(*rows)]
+        for rows in zip(*matrices)
+    ]
