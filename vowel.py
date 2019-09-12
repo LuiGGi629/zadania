cdc891182f88ef55966e80456ffcb80750568e33
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-09-12 14:20:37 +0200

    with a for loop

--- vowel.py
+++ vowel.py
@@ -1,3 +1,7 @@
 def get_vowel_names(names):
     """Return a list containing all names given that start with a vowel."""
-    pass
+    vowel_names = []
+    for name in names:
+        if name[0].lower() in "aeiou":
+            vowel_names.append(name)
+    return vowel_names
