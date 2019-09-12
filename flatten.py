3e17db8abfc48e9cbf9579194dd6ca074a1d0568
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-09-12 16:18:31 +0200

    with a list comprehension

--- flatten.py
+++ flatten.py
@@ -1,7 +1,7 @@
 def flatten(matrix):
     """Return a flattened version of the given 2-D matrix (list-of-lists)."""
-    flattened = []
-    for row in matrix:
-        for item in row:
-            flattened.append(item)
-    return flattened
+    return [
+        item
+        for row in matrix
+        for item in row
+    ]
