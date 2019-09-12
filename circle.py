c0f90fb2cd91a4edda836b4d09c60ec6e9228808
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-09-12 08:18:38 +0200

    added repr, radius, diameter and area

--- circle.py
+++ circle.py
@@ -1,2 +1,12 @@
+import math
+
+
 class Circle:
     """Circle with radius, area, and diameter."""
+    def __init__(self, radius=1):
+        self.radius = radius
+        self.area = math.pi * self.radius ** 2
+        self.diameter = self.radius * 2
+
+    def __repr__(self):
+        return f"Circle({self.radius})"
