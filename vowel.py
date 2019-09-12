396aeceafd6ae2c5359e9d4d16adc8d74e4229fe
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-09-12 14:22:32 +0200

    with a list comprehension

--- vowel.py
+++ vowel.py
@@ -1,7 +1,7 @@
 def get_vowel_names(names):
     """Return a list containing all names given that start with a vowel."""
-    vowel_names = []
-    for name in names:
-        if name[0].lower() in "aeiou":
-            vowel_names.append(name)
-    return vowel_names
+    return [
+        name
+        for name in names
+        if name[0].lower() in "aeiou"
+    ]
