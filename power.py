8ed461f6cf07a8a9b37b99fb72365054a9cf072c
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-09-12 15:31:33 +0200

    with enumerate and list comprehension

--- power.py
+++ power.py
@@ -1,6 +1,6 @@
 def power_list(numbers):
     """Return a list that contains each number raised to the i-th power."""
-    powers = []
-    for i, num in enumerate(numbers):
-        powers.append(num**i)
-    return powers
+    return [
+        num**i
+        for i, num in enumerate(numbers)
+    ]
