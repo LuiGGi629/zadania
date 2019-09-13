865f266b9d35ca5cde3e07e0bfdb38c4e6d6bdb9
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-09-13 09:57:49 +0200

    get_factors function with a list comp

--- factors.py
+++ factors.py
@@ -1,7 +1,7 @@
 def get_factors(number):
     """Return a list of all factors of the given number."""
-    factors = []
-    for num in range(1, number + 1):
-        if number % num == 0:
-            factors.append(num)
-    return factors
+    return [
+        num
+        for num in range(1, number + 1)
+        if number % num == 0
+    ]
