a58d9f42f4521d746cd9d8098d746f4a451dee4a
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-09-14 10:21:30 +0200

    pythagorean triples with a for loop

--- triples.py
+++ triples.py
@@ -1,3 +1,9 @@
 def triples(num):
     """Return list of Pythagorean triples less than input num."""
-    pass
+    triples_list = []
+    for a in range(1, num):
+        for b in range(a + 1, num):
+            for c in range(b + 1, num):
+                if a**2 + b**2 == c**2:
+                    triples_list.append((a, b, c))
+    return triples_list
