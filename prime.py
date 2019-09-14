fdd0ba7a7c2271d0eb449b851e2cf1bf43df7319
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-09-14 11:39:58 +0200

    is_prime with a generator expression and all

--- prime.py
+++ prime.py
@@ -1,6 +1,6 @@
 def is_prime(candidate):
     """Return True if candidate number is prime."""
-    return candidate >= 2 and not any(
-        candidate % num == 0
+    return candidate >= 2 and all(
+        candidate % num != 0
         for num in range(2, candidate)
     )
