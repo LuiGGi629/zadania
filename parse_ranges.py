5ae5112f33d7d1af5f5c0c63a6c33f35fddd0088
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-09-16 17:13:57 +0200

    parse_ranges function with two generator expressions

--- parse_ranges.py
+++ parse_ranges.py
@@ -1,10 +1,12 @@
 def parse_ranges(ranges_string):
     """Return a list of numbers corresponding to number ranges in a string."""
-    pairs = []
-    for group in ranges_string.split(","):
-        pairs.append(group.split("-"))
-    numbers = []
-    for start, stop in pairs:
-        for num in range(int(start), int(stop) + 1):
-            numbers.append(num)
-    return numbers
+    pairs = (
+        group.split("-")
+        for group in ranges_string.split(",")
+    )
+
+    return (
+        num
+        for start, stop in pairs
+        for num in range(int(start), int(stop) + 1)
+    )
