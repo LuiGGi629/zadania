6395349fe3eb8fa15b3034109f70855883f7eb7e
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-09-16 10:37:53 +0200

    with a for loop, doesn't pass test_empty_lists test

--- interleave.py
+++ interleave.py
@@ -1,2 +1,7 @@
 def interleave(iterable1, iterable2):
     """Return iterable of one item at a time from each list."""
+    interleaved = []
+    for item1, item2 in zip(iterable1, iterable2):
+        interleaved.append(item1)
+        interleaved.append(item2)
+    return interleaved
