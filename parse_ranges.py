faa2bde3e3655b044a535ad1522d3d7cf2e414ff
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-09-16 16:53:52 +0200

    parse_ranges function with a single for loop

--- parse_ranges.py
+++ parse_ranges.py
@@ -1,2 +1,8 @@
 def parse_ranges(ranges_string):
     """Return a list of numbers corresponding to number ranges in a string."""
+    numbers = []
+    for group in ranges_string.split(","):
+        start, stop = group.split("-")
+        for num in range(int(start), int(stop) + 1):
+            numbers.append(num)
+    return numbers
