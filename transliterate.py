4e4ca10522b728fd331e34a5ff002f2c8a4b52e7
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-09-16 13:53:01 +0200

    transliterate function with string concatenation

--- transliterate.py
+++ transliterate.py
@@ -1,2 +1,16 @@
+words = {
+    "esta": "is",
+    "la": "the",
+    "en": "in",
+    "rato": "rat",
+    "el": "the",
+    "casa": "house",
+}
+
+
 def transliterate(sentence):
     """Return a transliterated version of the given sentence."""
+    transliterated = ""
+    for word in sentence.split():
+        transliterated += words[word] + " "
+    return transliterated.rstrip()
