d695e812151688897b48d561f5d5ad14c927f89f
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-09-17 08:34:16 +0200

    first_prime_over function with a generator expression and next call

--- first_prime_over.py
+++ first_prime_over.py
@@ -3,6 +3,9 @@ from prime import is_prime
 
 def first_prime_over(number):
     """Return the first prime number over a given number."""
-    for number in range(number + 1, number**2):
-        if is_prime(number):
-            return number
+    primes = (
+        number
+        for number in range(number + 1, number**2)
+        if is_prime(number)
+    )
+    return next(primes)
