384f1671abbd2b3ac7079a8149b9e6c2c93fb892
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-09-17 08:36:15 +0200

    first_prime_over function with a genexpr passed directly into next

--- first_prime_over.py
+++ first_prime_over.py
@@ -3,9 +3,8 @@ from prime import is_prime
 
 def first_prime_over(number):
     """Return the first prime number over a given number."""
-    primes = (
+    return next(
         number
         for number in range(number + 1, number**2)
         if is_prime(number)
     )
-    return next(primes)
