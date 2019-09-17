0ade1047c8875b3ab8f47448ecd074bd04278798
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-09-17 10:00:36 +0200

    flip_dict function with a dictionary comprehension

--- flip_dict.py
+++ flip_dict.py
@@ -1,6 +1,6 @@
 def flip_dict(dictionary):
     """Return a new dictionary that maps the original values to the keys."""
-    flipped = {}
-    for old_key, old_value in dictionary.items():
-        flipped[old_value] = old_key
-    return flipped
+    return {
+        old_value: old_key
+        for old_key, old_value in dictionary.items()
+    }
