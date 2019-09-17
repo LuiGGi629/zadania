d13e601f448c0f4f74d168f600ae00231dd8bfdc
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-09-17 08:29:34 +0200

    first_prime_over function with early break

---
+++ first_prime_over.py
@@ -0,0 +1,8 @@
+from prime import is_prime
+
+
+def first_prime_over(number):
+    """Return the first prime number over a given number."""
+    for number in range(number + 1, number**2):
+        if is_prime(number):
+            return number
