4469111cd5f40d0ea3a0712121efddb1d1ee164e
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-09-18 13:11:47 +0200

    get_ascii_codes without a list comp inside a dict comprehension

--- ascii.py
+++ ascii.py
@@ -1,6 +1,6 @@
 def get_ascii_codes(words):
     """Return a dictionary mapping the strings to ASCII codes."""
-    codes = {}
-    for word in words:
-        codes[word] = [ord(char) for char in word]
-    return codes
+    return {
+        word: [ord(char) for char in word]
+        for word in words
+    }
