47c2c7ca47ca41c84e1cfe85cadf3b06d98324d7
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-09-19 07:58:32 +0200

    more idiomatic with tuple unpacking

--- dict_from_truple.py
+++ dict_from_truple.py
@@ -1,6 +1,6 @@
 def dict_from_truple(input_list):
     """Turn three-item tuples into a dicitonary of two-valued tuples."""
-    new_dict = {}
-    for tup in input_list:
-        new_dict[tup[0]] = (tup[1], tup[2])
-    return new_dict
+    truple_dict = {}
+    for key, value1, value2 in input_list:
+        truple_dict[key] = (value1, value2)
+    return truple_dict
