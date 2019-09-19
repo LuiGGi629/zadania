e65bbae11d18a55a9da3253719dc8e00b8b52d4e
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-09-19 16:34:28 +0200

    matrix_from_string function with a comprehension for inner for loop

--- matrix_from_string.py
+++ matrix_from_string.py
@@ -2,8 +2,5 @@ def matrix_from_string(string):
     """Convert rows of numbers to list of lists."""
     matrix = []
     for line in string.splitlines():
-        row = []
-        for x in line.split():
-            row.append(int(x))
-        matrix.append(row)
+        matrix.append([int(x) for x in line.split()])
     return matrix
