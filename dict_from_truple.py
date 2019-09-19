7d87a3c78db7ba88e258b8565cd03bdeafec8310
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-09-19 08:00:19 +0200

    dict_from_truple function with a dictionary comprehension

--- dict_from_truple.py
+++ dict_from_truple.py
@@ -1,6 +1,6 @@
 def dict_from_truple(input_list):
     """Turn three-item tuples into a dicitonary of two-valued tuples."""
-    truple_dict = {}
-    for key, value1, value2 in input_list:
-        truple_dict[key] = (value1, value2)
-    return truple_dict
+    return {
+        key: (value1, value2)
+        for key, value1, value2 in input_list
+    }
