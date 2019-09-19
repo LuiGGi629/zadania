fe0c4c6c2a50e93c6b57992cedecdd7fc7b71774
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-09-19 07:54:18 +0200

    dict_from_truple function without tuple unpacking

--- dict_from_truple.py
+++ dict_from_truple.py
@@ -1,6 +1,6 @@
 def dict_from_truple(input_list):
     """Turn three-item tuples into a dicitonary of two-valued tuples."""
     new_dict = {}
-    for items in input_list:
-        new_dict[items[0]] = items[1:]
+    for tup in input_list:
+        new_dict[tup[0]] = (tup[1], tup[2])
     return new_dict
--- test_dict_from_truple.py
+++ test_dict_from_truple.py
@@ -9,11 +9,6 @@ class DictFromTupleTests(unittest.TestCase):
         outputs = {1: (2, 3), 4: (5, 6), 7: (8, 9)}
         self.assertEqual(dict_from_truple(inputs), outputs)
 
-    def test_four_items(self):
-        inputs = [(1, 2, 3, 4), (5, 6, 7, 8), (9, 10, 11, 12)]
-        outputs = {1: (2, 3, 4), 5: (6, 7, 8), 9: (10, 11, 12)}
-        self.assertEqual(dict_from_truple(inputs), outputs)
-
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
