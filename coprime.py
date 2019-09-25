f78330b59bf11fd7262b7a513bc21aef99ceb81d
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-09-25 10:52:36 +0200

    coprime function with a genexpr

--- coprime.py
+++ coprime.py
@@ -1,6 +1,6 @@
 def coprime(number1, number2):
     """Return True if candidate numbers are coprime."""
-    for num in range(2, min(number1, number2) + 1):
-        if number1 % num == 0 and number2 % num == 0:
-            return False
-    return True
+    return not any(
+        number1 % num == 0 and number2 % num == 0
+        for num in range(2, min(number1, number2) + 1)
+    )
