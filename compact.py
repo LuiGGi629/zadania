5b4233f7a46d26b5e8a7e84fde484e9b0f314958
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-09-26 11:15:14 +0200

    loop over iterable once and not depend on indexing

--- compact.py
+++ compact.py
@@ -1,11 +1,11 @@
 def compact(iterable):
     """Return iterable with adjacent duplicate values removed."""
-    sequence = list(iterable)
-    # converting incoming iterable to a list
     deduplicated = []
-    # this isn't very efficient -> creating a new list
-    # just to loop over it once and discard it
-    for i, item in enumerate(sequence):
-        if i == 0 or item != sequence[i-1]:
+    previous = None
+    # loop over iterable once and not depend on indexing
+    for item in iterable:
+        if item != previous:
             deduplicated.append(item)
+            previous = item
     return deduplicated
+    # this doesn't work because list starts with None values
