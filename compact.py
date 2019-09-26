5372b23664135a972f4e21a408dd148547a3c926
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-09-26 10:45:32 +0200

    compact function with zip and *"unpacking"

--- compact.py
+++ compact.py
@@ -1,10 +1,10 @@
 def compact(sequence):
     """Return iterable with adjacent duplicate values removed."""
     deduplicated = []
-    for i, item in enumerate(sequence):
-        # use indexes to check whether the current item is the same as before
-        if i == 0 or item != sequence[i-1]:
-            # appending item 0 and then only appending subsequent items
-            # if they are not equal to the item just before their index
+    # zip together the original sequence with itself shifted by one
+    # so that the each item can compare itself with the one before
+    for item, previous in zip(sequence, [object(), *sequence]):
+        # *"unpacking" each of the items into new list, after object()
+        if item != previous:
             deduplicated.append(item)
     return deduplicated
