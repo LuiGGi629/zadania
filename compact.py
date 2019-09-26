9498e490bfbb7ef90a1e0e7c07e824fc8a469e7b
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-09-26 11:44:39 +0200

    setting previous variable to a value that is only every equal to itself

--- compact.py
+++ compact.py
@@ -1,11 +1,10 @@
 def compact(iterable):
     """Return iterable with adjacent duplicate values removed."""
     deduplicated = []
-    previous = None
-    # loop over iterable once and not depend on indexing
+    previous = object()
+    # setting variable to a value that is only every equal to itself
     for item in iterable:
         if item != previous:
             deduplicated.append(item)
             previous = item
     return deduplicated
-    # this doesn't work because list starts with None values
