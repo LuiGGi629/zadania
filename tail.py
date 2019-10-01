e8ee1de71597b41c5b61d2103604585198f2dbcc
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-10-01 10:54:58 +0200

    max dq with iterable, set max length, convert dq to a list

--- tail.py
+++ tail.py
@@ -5,7 +5,4 @@ def tail(iterable, n):
     """Return the last n items of given sentence."""
     if n <= 0:
         return []
-    items = deque(maxlen=n)
-    for item in iterable:
-        items.append(item)
-    return list(items)
+    return list(deque(iterable, maxlen=n))
