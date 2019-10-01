aa6d42bc2c8c4b2b30adef342facf57820dae29e
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-10-01 10:32:44 +0200

    U don't see slice objects used much directly in Python

--- tail.py
+++ tail.py
@@ -4,7 +4,9 @@ def tail(iterable, n):
     if n <= 0:
         return []
     elif n == 1:
+        # slice object is what Python creates when U use slicing notation
         index = slice(0, 0)
+        # when U say sequence[-n:] == sequence[slice(-n, None)]
     else:
         index = slice(-(n-1), None)
     for item in iterable:
