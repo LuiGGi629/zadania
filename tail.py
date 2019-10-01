9385b10200f0f5302b5e90c98790cc6433ea9ee6
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-10-01 10:13:53 +0200

    move if- else before for loop by sticking for loop inside it

--- tail.py
+++ tail.py
@@ -1,11 +1,10 @@
 def tail(iterable, n):
     """Return the last n items of given sentence."""
     items = []
-    if n <= 0:
-        return []
-    for item in iterable:
-        if n == 1:
+    if n == 1:
+        for item in iterable:
             items = [item]
-        else:
+    elif n > 0:
+        for item in iterable:
             items = [*items[-n+1:], item]
     return items
