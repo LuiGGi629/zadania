c877e820d9fde33b77340f1f16b8fe74a57a2a8f
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-10-04 11:31:20 +0200

    if statement without an else

--- count.py
+++ count.py
@@ -2,8 +2,7 @@ def count_words(string):
     """Return the number of items each word occurs in the string."""
     count = {}
     for word in string.split():
-        if word in count:
-            count[word] += 1
-        else:
-            count[word] = 1
+        if word not in count:
+            count[word] = 0
+        count[word] += 1
     return count
