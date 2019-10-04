fc5808bfea7a82dcd3131ba31df8574245fd2400
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-10-04 11:38:53 +0200

    setdefault to add key-val pair with default val if key not in dict

--- count.py
+++ count.py
@@ -2,5 +2,6 @@ def count_words(string):
     """Return the number of items each word occurs in the string."""
     count = {}
     for word in string.split():
-        count[word] = count.get(word, 0) + 1
+        count.setdefault(word, 0)
+        count[word] += 1
     return count
