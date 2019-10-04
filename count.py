a6f37fd3633f490f7f87bd98eb0dc4b2be3e41bf
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-10-04 13:45:54 +0200

    normalize the case by lowercasing all the words

--- count.py
+++ count.py
@@ -3,4 +3,4 @@ from collections import Counter
 
 def count_words(string):
     """Return the number of items each word occurs in the string."""
-    return Counter(string.split())
+    return Counter(string.lower().split())
