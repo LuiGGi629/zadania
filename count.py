10a8f8bbcbd57c0301edbb159c21d900db7a00b7
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-10-04 14:00:53 +0200

    more accurate with word breaks in regular expression

--- count.py
+++ count.py
@@ -4,4 +4,4 @@ import re
 
 def count_words(string):
     """Return the number of items each word occurs in the string."""
-    return Counter(re.findall(r"[\w'-]+", string.lower()))
+    return Counter(re.findall(r"\b[\w'-]+\b", string.lower()))
