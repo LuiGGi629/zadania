31ec1664ab669358745928d50f3fb56cd5b9e9a8
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-10-04 11:35:02 +0200

    get method to lookup value for a key or get specific if not key in dict

--- count.py
+++ count.py
@@ -2,7 +2,5 @@ def count_words(string):
     """Return the number of items each word occurs in the string."""
     count = {}
     for word in string.split():
-        if word not in count:
-            count[word] = 0
-        count[word] += 1
+        count[word] = count.get(word, 0) + 1
     return count
