9acf262f30db5002949b73c62917c57533a39067
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-10-04 13:50:33 +0200

    strip punctuaction characters from the ends of words after looping

--- count.py
+++ count.py
@@ -3,4 +3,7 @@ from collections import Counter
 
 def count_words(string):
     """Return the number of items each word occurs in the string."""
-    return Counter(string.lower().split())
+    words = []
+    for word in string.lower().split():
+        words.append(word.strip(',.?";!()¿'))
+    return Counter(words)
