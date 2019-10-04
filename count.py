f555f06181d3701113f201faa138d677ce89198d
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-10-04 13:44:15 +0200

    Counter - set that counts num of times it sees each thing

--- count.py
+++ count.py
@@ -1,9 +1,6 @@
-from collections import defaultdict
+from collections import Counter
 
 
 def count_words(string):
     """Return the number of items each word occurs in the string."""
-    count = defaultdict(int)
-    for word in string.split():
-        count[word] += 1
-    return count
+    return Counter(string.split())
