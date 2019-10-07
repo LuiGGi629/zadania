f67be934d0f2f9d6859a37272b74915407c325a9
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-10-07 12:23:13 +0200

    doesn't handle quotes when delimiters are in the output data

---
+++ csv/fix_csv.py
@@ -0,0 +1,21 @@
+#! /usr/local/bin/python3
+import sys
+
+old_filename = sys.argv[1]
+new_filename = sys.argv[2]
+
+old_file = open(old_filename)
+
+rows = [
+    line.split("|")
+    for line in old_file.read().splitlines()
+]
+
+new_file = open(new_filename, mode="wt", newline="\r\n")
+print("\n".join(
+    ",".join(row)
+    for row in rows
+), file=new_file)
+
+old_file.close()
+new_file.close()
