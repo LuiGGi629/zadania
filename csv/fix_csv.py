df48621b5cb55263246713ff4bb4158bd0be300b
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-10-08 09:41:09 +0200

    this removes all conditional checking and exceptional cases

--- csv/fix_csv.py
+++ csv/fix_csv.py
@@ -5,18 +5,12 @@ import csv
 parser = ArgumentParser()
 parser.add_argument("old_filename")
 parser.add_argument("new_filename")
-parser.add_argument("--in-delimiter", dest="delim")
-parser.add_argument("--in-quote", dest="quote")
+parser.add_argument("--in-delimiter", dest="delim", default='|')
+parser.add_argument("--in-quote", dest="quote", default='"')
 args = parser.parse_args()
 
 with open(args.old_filename, newline="") as old_file:
-    quotechar = '"'
-    delimiter = '|'
-    if args.delim:
-        delimiter = args.delim
-    if args.quote:
-        quotechar = args.quote
-    reader = csv.reader(old_file, delimiter=delimiter, quotechar=quotechar)
+    reader = csv.reader(old_file, delimiter=args.delim, quotechar=args.quote)
     rows = list(reader)
 
 with open(args.new_filename, mode="wt", newline="") as new_file:
