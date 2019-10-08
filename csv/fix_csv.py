d4fb54bf540d67ba1114bed8f87ad07e407e09da
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-10-08 09:50:10 +0200

    attempt to auto detect the format of input file

--- csv/fix_csv.py
+++ csv/fix_csv.py
@@ -5,12 +5,20 @@ import csv
 parser = ArgumentParser()
 parser.add_argument("old_filename")
 parser.add_argument("new_filename")
-parser.add_argument("--in-delimiter", dest="delim", default='|')
-parser.add_argument("--in-quote", dest="quote", default='"')
+parser.add_argument("--in-delimiter", dest="delim")
+parser.add_argument("--in-quote", dest="quote")
 args = parser.parse_args()
 
 with open(args.old_filename, newline="") as old_file:
-    reader = csv.reader(old_file, delimiter=args.delim, quotechar=args.quote)
+    arguments = {}
+    if args.delim:
+        arguments["delimiter"] = args.delim
+    if args.quote:
+        arguments["quotechar"] = args.quote
+    if not args.delim and not args.quote:
+        arguments["dialect"] = csv.Sniffer().sniff(old_file.read())
+        old_file.seek(0)
+    reader = csv.reader(old_file, **arguments)
     rows = list(reader)
 
 with open(args.new_filename, mode="wt", newline="") as new_file:
