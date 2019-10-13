64213aa8b088f9a5f21e5ed3ec6e39affdc627a6
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-10-13 09:54:57 +0200

    tuples to compare instead of strings - lexicographical ordering

--- Earliest/earliest.py
+++ Earliest/earliest.py
@@ -3,10 +3,7 @@ def get_earliest(date1, date2):
     month1, day1, year1 = date1.split('/')
     month2, day2, year2 = date2.split('/')
 
-    ymd1 = year1 + month1 + day1
-    ymd2 = year2 + month2 + day2
-
-    if ymd1 < ymd2:
+    if (year1, month1, day1) < (year2, month2, day2):
         return date1
     else:
         return date2
