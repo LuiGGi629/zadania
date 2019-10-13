445bd2b71881a5f63a7a2cda78adb80544efec62
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-10-13 10:01:29 +0200

    regexp - 2 digits, 2 digits, 4 digits with slashes in between them

--- Earliest/earliest.py
+++ Earliest/earliest.py
@@ -1,6 +1,10 @@
+import re
+
+
 def get_earliest(date1, date2):
     """Return earlies of two MM/DD/YYYY-formatted date strings."""
-    (month1, day1, year1) = date1.split('/')
-    (month2, day2, year2) = date2.split('/')
+    DATE_RE = re.compile(r'^(\d{2})/(\d{2})/(\d{4}$)')
+    (month1, day1, year1) = DATE_RE.search(date1).groups()
+    (month2, day2, year2) = DATE_RE.search(date2).groups()
 
     return date1 if (year1, month1, day1) < (year2, month2, day2) else date2
