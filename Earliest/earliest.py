d7c2b14dcc31ef45cb46da47aea3e15764198da3
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-10-13 07:39:19 +0200

    join together YYYYMMDD strings to compare

--- Earliest/earliest.py
+++ Earliest/earliest.py
@@ -7,17 +7,10 @@ def get_earliest(date1, date2):
     day2 = date2[3:5]
     year2 = date2[6:]
 
-    if year1 < year2:
+    ymd1 = year1 + month1 + day1
+    ymd2 = year2 + month2 + day2
+
+    if ymd1 < ymd2:
         return date1
-    elif year1 > year2:
-        return date2
-    elif month1 < month2:
-        return date1
-    elif month1 > month2:
-        return date2
-    elif day1 < day2:
-        return date1
-    elif day1 > day2:
-        return date2
     else:
-        return date1
+        return date2
