bd351525f7c2448d46179e63d85e342b99001284
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-10-13 09:50:59 +0200

    tuple unpacking to make those date variables more easily

--- Earliest/earliest.py
+++ Earliest/earliest.py
@@ -1,13 +1,7 @@
 def get_earliest(date1, date2):
     """Return earlies of two MM/DD/YYYY-formatted date strings."""
-    date1_split = date1.split('/')
-    date2_split = date2.split('/')
-    month1 = date1_split[0]
-    day1 = date1_split[1]
-    year1 = date1_split[2]
-    month2 = date2_split[0]
-    day2 = date2_split[1]
-    year2 = date2_split[2]
+    month1, day1, year1 = date1.split('/')
+    month2, day2, year2 = date2.split('/')
 
     ymd1 = year1 + month1 + day1
     ymd2 = year2 + month2 + day2
