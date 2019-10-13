f4db005c8e1301c7fcdfc3fab7e26791db8fcfb3
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-10-13 07:28:18 +0200

    series of if-elif-else statements and string slicing

--- Earliest/earliest.py
+++ Earliest/earliest.py
@@ -1,2 +1,16 @@
-def get_earliest():
-    pass
+def get_earliest(date1, date2):
+    """Return earlies of two MM/DD/YYYY-formatted date strings."""
+    if date1[6:] < date2[6:]:
+        return date1
+    elif date1[6:] > date2[6:]:
+        return date2
+    elif date1[2:] < date2[2:]:
+        return date1
+    elif date1[2:] > date2[2:]:
+        return date2
+    elif date1[3:5] < date2[3:5]:
+        return date1
+    elif date1[3:5] > date2[3:5]:
+        return date2
+    else:
+        return date1
