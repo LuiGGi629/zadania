79767ca2eded6da923e8a1251631212d4c03c054
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-10-13 07:36:13 +0200

    variables names to describe what we're comparing

--- Earliest/earliest.py
+++ Earliest/earliest.py
@@ -1,16 +1,23 @@
 def get_earliest(date1, date2):
     """Return earlies of two MM/DD/YYYY-formatted date strings."""
-    if date1[6:] < date2[6:]:
+    month1 = date1[:2]
+    day1 = date1[3:5]
+    year1 = date1[6:]
+    month2 = date2[:2]
+    day2 = date2[3:5]
+    year2 = date2[6:]
+
+    if year1 < year2:
         return date1
-    elif date1[6:] > date2[6:]:
+    elif year1 > year2:
         return date2
-    elif date1[2:] < date2[2:]:
+    elif month1 < month2:
         return date1
-    elif date1[2:] > date2[2:]:
+    elif month1 > month2:
         return date2
-    elif date1[3:5] < date2[3:5]:
+    elif day1 < day2:
         return date1
-    elif date1[3:5] > date2[3:5]:
+    elif day1 > day2:
         return date2
     else:
         return date1
