2585e42b42adb786cb520b8d1481778fa446b225
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-10-13 09:57:09 +0200

    inline "if" statement

--- Earliest/earliest.py
+++ Earliest/earliest.py
@@ -1,9 +1,6 @@
 def get_earliest(date1, date2):
     """Return earlies of two MM/DD/YYYY-formatted date strings."""
-    month1, day1, year1 = date1.split('/')
-    month2, day2, year2 = date2.split('/')
+    (month1, day1, year1) = date1.split('/')
+    (month2, day2, year2) = date2.split('/')
 
-    if (year1, month1, day1) < (year2, month2, day2):
-        return date1
-    else:
-        return date2
+    return date1 if (year1, month1, day1) < (year2, month2, day2) else date2
