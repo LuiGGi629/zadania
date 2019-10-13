1d068e1a9a0451304e3d40752e20db9d82f79bd3
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-10-13 10:39:23 +0200

    added comments because there's a lot going on

--- Earliest/earliest.py
+++ Earliest/earliest.py
@@ -1,6 +1,12 @@
 def get_earliest(*dates):
-    """Return earlies of two MM/DD/YYYY-formatted date strings."""
+    """Return earliest of two MM/DD/YYYY-formatted date strings.
+    Providing a key keyword argument. Key function tells the min function
+    what criteria determining whether one date string is less than
+    (earlier than) another one. The key function will be called with
+    each date string and the returned date string will be the one
+    with resulting key tuple which is smalles."""
     def date_key(date):
         (month, day, year) = date.split('/')
+        # split and rearrange each date string
         return (year, month, day)
-    return min(dates, key=date_key)
+    return min(dates, key=date_key)  # find out which one is the earliest
