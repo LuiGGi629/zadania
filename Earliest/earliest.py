5c40dc4ff69a84f18c78493f67706ebdb42cd967
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2019-10-13 09:43:02 +0200

    split these strings on "/"

--- Earliest/earliest.py
+++ Earliest/earliest.py
@@ -1,11 +1,13 @@
 def get_earliest(date1, date2):
     """Return earlies of two MM/DD/YYYY-formatted date strings."""
-    month1 = date1[:2]
-    day1 = date1[3:5]
-    year1 = date1[6:]
-    month2 = date2[:2]
-    day2 = date2[3:5]
-    year2 = date2[6:]
+    date1_split = date1.split('/')
+    date2_split = date2.split('/')
+    month1 = date1_split[0]
+    day1 = date1_split[1]
+    year1 = date1_split[2]
+    month2 = date2_split[0]
+    day2 = date2_split[1]
+    year2 = date2_split[2]
 
     ymd1 = year1 + month1 + day1
     ymd2 = year2 + month2 + day2
